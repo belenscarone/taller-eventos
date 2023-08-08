0df0d7200f820db622a8e1aa32f1b20bce3646f2
belenscarone <sbscarone@gmail.com>
2023-08-08 00:48:22 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
 const getDiv = document.getElementById("buttonDiv");
+const mybutton = getDiv.querySelector("button");
+
+mybutton.addEventListener("click", function(event){
+    event.stopPropagation();
+});
+
 getDiv.addEventListener("click", function(){
     alert("Hola! Soy el div");
 });
