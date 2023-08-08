4ac65fac5bc42d5690180bc471ea9f12069b3070
belenscarone <sbscarone@gmail.com>
2023-08-07 23:20:54 -0300

    commit

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+const getDiv = document.getElementById("buttonDiv");
+getDiv.addEventListener("click", function(){
+    alert("Hola! Soy el div");
+});
+});
